cab66195a3db50edb77164fc892d606bd99199f6
Chepkoech Toweett <chepie@Chepkoechs-MacBook-Air.local>
2023-10-28 23:48:05 +0300

    fixed the photos view and controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,8 +1,36 @@
 class PhotosController < ApplicationController
+  before_action :set_user, only: [:create]
+  
+  def index
+    @photos = Photo.all # or Photo.where(some_condition) based on your requirements
+  end
+
   def new
     @photo = Photo.new
-  end  
-  
+  end
+
+  def create
+    @photo = @user.photos.build(photo_params)
+    if @photo.save
+      redirect_to @photo, notice: 'Photo was successfully created.'
+    else
+      render :new
+    end
+  end
+
   def show
+    @photo = Photo.find(params[:id])
+  end
+
+  private
+
+  def set_user
+    # Fetch the current user
+    @user = current_user
+  end
+
+  def photo_params
+    # Permit necessary attributes and the association with the user
+    params.require(:photo).permit(:attribute1, :attribute2, :attribute3)
   end
 end
