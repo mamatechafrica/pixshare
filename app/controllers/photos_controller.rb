22634cdc4ee81bf5d154d76af72a0a9fc7e36335
Chepkoech Toweett <chepie@Chepkoechs-MacBook-Air.local>
2023-11-08 21:52:39 +0300

    added grid to show photo with delete button

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -27,16 +27,48 @@ class PhotosController < ApplicationController
   # Add a new action to handle photo deletion
   def destroy
     @photo = Photo.find(params[:id])
+    image_id_to_delete = params[:image_id].to_i
 
     # Check if the current user is the owner of the photo
     if @photo.user == current_user
-      # Remove the photo from the database
-      @photo.destroy
-      redirect_to photos_path, notice: 'Photo was successfully deleted.'
+      # Find and delete the image by its ID or index
+      if @photo.images[image_id_to_delete].present?
+        @photo.images[image_id_to_delete].purge
+        redirect_to @photo, notice: 'Image was successfully deleted.'
+      else
+        redirect_to @photo, alert: 'Image not found.'
+      end
     else
-      # If the current user is not the owner, you can handle this as you see fit
-      redirect_to @photo, alert: 'You are not authorized to delete this photo.'
+      # If the current user is not the owner, handle this as needed
+      redirect_to @photo, alert: 'You are not authorized to delete images from this photo.'
     end
+  end
+
+  def update
+    @photo = Photo.find(params[:id])
+
+    # Check if the current user is the owner of the photo
+    if @photo.user == current_user
+      # Start a transaction in case something fails
+      ActiveRecord::Base.transaction do
+        # Process any new images for attachment
+        @photo.images.attach(params[:photo][:images]) if params[:photo][:images]
+
+        # Update the photo's attributes
+        raise ActiveRecord::Rollback unless @photo.update(photo_params.except(:images))
+
+        redirect_to @photo, notice: 'Photo was successfully updated.'
+
+        # Rollback the transaction if update fails
+      end
+    else
+      # If the current user is not the owner, handle this as needed
+      redirect_to @photo, alert: 'You are not authorized to edit this photo.'
+    end
+  end
+
+  def edit
+    @photo = Photo.find(params[:id])
   end
 
   private
