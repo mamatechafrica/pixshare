1cc11fcf13b0634af9490d398513e16064d696a0
Adrian <adrian@adrianthedev.com>
2023-11-19 20:41:49 +0200

    add name and email

--- db/migrate/20231022160216_create_users.rb
+++ db/migrate/20231022160216_create_users.rb
@@ -4,7 +4,8 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def self.up
     create_table :users do |t|
       ## Database authenticatable
-      # t.string :email,              null: false, default: ""
+      t.string :name,               null: false, default: ""
+      t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ''
 
       ## Recoverable
