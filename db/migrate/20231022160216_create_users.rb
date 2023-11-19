f9813f80c5103dfc317d63be794181e970d6666f
Adrian <adrian@adrianthedev.com>
2023-11-19 20:41:00 +0200

    use the original migration

--- db/migrate/20231022160216_create_users.rb
+++ db/migrate/20231022160216_create_users.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddDeviseToUsers < ActiveRecord::Migration[7.0]
+class CreateUsers < ActiveRecord::Migration[7.0]
   def self.up
     create_table :users do |t|
       ## Database authenticatable
